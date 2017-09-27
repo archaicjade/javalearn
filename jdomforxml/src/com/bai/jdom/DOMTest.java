86df7996344f59c1a9f1a3351065a11686132053
big ball <safe.baiyu@yahoo.com>
2017-09-27 16:02:52 +0800

    jdom create xml

--- jdomforxml/src/com/bai/jdom/DOMTest.java
+++ jdomforxml/src/com/bai/jdom/DOMTest.java
@@ -2,19 +2,21 @@ package com.bai.jdom;
 
 import java.io.IOException;
 
-import org.jdom2.JDOMException;
-
 public class DOMTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		try {
-			JDOMHelper.parseXML();
-		} catch (JDOMException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
+			// JDOMHelper.parseXML();
+			JDOMHelper.createXml();
+
+		} 
+		// catch (JDOMException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+		catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
--- jdomforxml/src/com/bai/jdom/JDOMHelper.java
+++ jdomforxml/src/com/bai/jdom/JDOMHelper.java
@@ -2,17 +2,39 @@ package com.bai.jdom;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
+import org.jdom2.Attribute;
+import org.jdom2.CDATA;
 import org.jdom2.Document;
 import org.jdom2.Element;
-import org.jdom2.Attribute;
 import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
 
 public class JDOMHelper {
+
+	public static void createXml() throws FileNotFoundException, IOException {
+
+		Element rss = new Element("rss");
+
+		rss.setAttribute("version", "2.0");
+		Document doc = new Document(rss);
+
+		rss.addContent(new Element("channel").addContent(new Element("title").addContent(new CDATA("国内最新新闻"))));
+
+		Format f = Format.getPrettyFormat();
+
+		XMLOutputter outputer = new XMLOutputter(f);
+
+		outputer.output(doc, new FileOutputStream(new File("src/res/rssnew.xml")));
+
+	}
 
 	public static void parseXML() throws JDOMException, IOException {
 		SAXBuilder builder = new SAXBuilder();
